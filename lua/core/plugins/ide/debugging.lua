80322e7437cbeade084b7c03ccb01710638ef5ca
oskaryanders <oskaryaandersson@gmail.com>
2025-03-24 20:54:40 +0100

    Improving

--- lua/core/plugins/ide/debugging.lua
+++ lua/core/plugins/ide/debugging.lua
@@ -2,5 +2,40 @@ return {
   {
     'mfussenegger/nvim-dap',
     lazy = true,
+    config = function()
+      local dap = require 'dap'
+
+      dap.adapters.python = {
+        type = 'executable',
+        command = 'python',
+        args = { '-m', 'debugpy.adapter' },
+      }
+
+      dap.configurations.python = {
+        {
+          type = 'python',
+          request = 'launch',
+          name = 'Launch file',
+          program = '${file}',
+          pythonPath = function()
+            return 'python' -- Adjust if using a virtual environment
+          end,
+        },
+      }
+    end,
+  },
+  {
+    'rcarriga/nvim-dap-ui',
+    dependencies = { 'mfussenegger/nvim-dap' },
+    config = function()
+      require('dapui').setup()
+    end,
+  },
+  {
+    'mfussenegger/nvim-dap-python',
+    dependencies = { 'mfussenegger/nvim-dap' },
+    config = function()
+      require('dap-python').setup 'python' -- Adjust path if needed
+    end,
   },
 }
--- lua/core/plugins/ide/git.lua
+++ lua/core/plugins/ide/git.lua
@@ -1,6 +1,22 @@
 -- gitsigns for showing blame and what has been edited.
 -- https://github.com/lewis6991/gitsigns
 
-return { {
-  'lewis6991/gitsigns.nvim',
-} }
+return {
+  {
+    'lewis6991/gitsigns.nvim',
+    opts = {
+      signs = {
+        add = { text = '│' },
+        change = { text = '│' },
+        delete = { text = '_' },
+        topdelete = { text = '‾' },
+        changedelete = { text = '~' },
+        untracked = { text = '┆' },
+      },
+      signcolumn = true, -- Show signs in the sign column
+      numhl = false, -- Disable line number highlighting
+      linehl = false, -- Disable full line highlight
+      word_diff = false, -- Disable word diff highlighting
+    },
+  },
+}
